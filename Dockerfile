7f553f3a65b7f612882275c4e7fe7861a5fa3807
goflutterjava <goflutterjava@gmail.com>
2023-02-13 17:34:42 +0800

    use proper influxdb config

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM ttbb/influxdb1:nake
 
 COPY docker-build /opt/influxdb/mate
 
-COPY config/influxdb.conf /opt/influxdb/etc/influxdb/influxdb.conf
-
 WORKDIR /opt/influxdb
 
 CMD ["/usr/bin/dumb-init", "bash", "-vx", "/opt/influxdb/mate/scripts/start.sh"]
--- docker-build/scripts/start-influx.sh
+++ docker-build/scripts/start-influx.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 mkdir -p $INFLUX_HOME/logs
+CONF_FILE=$INFLUX_HOME/etc/influxdb/influxdb.conf
+echo "[meta]" >$CONF_FILE
+echo '  dir = "/opt/influxdb/meta"' >>$CONF_FILE
+echo "[data]" >>$CONF_FILE
+echo '  dir = "/opt/influxdb/data"' >>$CONF_FILE
+echo '  wal-dir = "/opt/influxdb/wal"' >>$CONF_FILE
+echo '  series-id-set-cache-size = 100' >>$CONF_FILE
+echo "[http]" >>$CONF_FILE
+echo '  flux-enabled = true' >>$CONF_FILE
 nohup influxd run -config $INFLUX_HOME/etc/influxdb/influxdb.conf >>$INFLUX_HOME/logs/influxdb1.stdout.log 2>>$INFLUX_HOME/logs/influxdb1.stderr.log &
